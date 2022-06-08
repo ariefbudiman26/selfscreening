a9fcf742e29f4f4e7eb86ce57cd1cfababb908e9
ariefbudiman26 <ariefbudiman2693@gmail.com>
2022-06-08 10:54:40 +0700

    upload asset and add theme

---
+++ lib/themes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 10.0;
+double defaultRadius = 10.0;
+
+Color backgroundColor = Color(0xff0F0F10);
+Color orangeColor = Colors.orange;
+Color purpleButtonColor = Color(0xff7339E5);
+Color purpletTextColor = Color(0xff804CEF);
+Color greyColumnColor = Color(0xff262629);
+Color borderColor = Color(0xff6A3535);
+Color greyTextColor = Color(0xff575758);
+Color whiteTextColor = Color(0xffffffff);
+
+TextStyle purpleTextStyle = GoogleFonts.inter(color: purpletTextColor);
+TextStyle whiteTextStyle = GoogleFonts.inter(color: whiteTextColor);
+TextStyle greyTextStyle = GoogleFonts.inter(color: greyTextColor);
