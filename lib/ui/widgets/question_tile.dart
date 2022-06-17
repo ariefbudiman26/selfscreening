478462be22264a88ca6d890bf1abbaf25046da8a
ariefbudiman26 <ariefbudiman2693@gmail.com>
2022-06-17 15:50:24 +0700

    edit screening pag

--- lib/ui/widgets/question_tile.dart
+++ lib/ui/widgets/question_tile.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:selfscreening/shared/themes.dart';
 
-class QuestionTile extends StatelessWidget {
+class QuestionTile extends StatefulWidget {
   final String question;
   const QuestionTile({Key? key, required this.question}) : super(key: key);
 
   @override
+  State<QuestionTile> createState() => _QuestionTileState();
+}
+
+class _QuestionTileState extends State<QuestionTile> {
+  int selectedValue = 0;
+
+  @override
   Widget build(BuildContext context) {
+    Widget radioButton() {
+      return Column(
+        children: [
+          RadioListTile<int>(
+            value: 1,
+            title: const Text('Yes'),
+            groupValue: selectedValue,
+            onChanged: (value) => setState(() {
+              selectedValue = 1;
+            }),
+          ),
+          RadioListTile<int>(
+            value: 2,
+            title: const Text('No'),
+            groupValue: selectedValue,
+            onChanged: (value) => setState(() {
+              selectedValue = 2;
+            }),
+          ),
+        ],
+      );
+    }
+
     return Container(
       padding: const EdgeInsets.all(20),
       width: MediaQuery.of(context).size.width / 1.2,
@@ -14,9 +44,14 @@ class QuestionTile extends StatelessWidget {
         color: Colors.amber,
         borderRadius: BorderRadius.circular(defaultRadius),
       ),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(question),
+          Text(widget.question),
+          SizedBox(
+            height: 10,
+          ),
+          radioButton(),
         ],
       ),
     );
